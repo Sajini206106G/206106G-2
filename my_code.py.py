64e112726078bb90fd7c794e3867c43d72b02e4d
sajini <sajinis.20@uom.lk>
2023-10-11 14:59:30 +0530

    Initial commit

---
+++ my_code.py.py
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>S.Sajini-206106G</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container-fluid bg-light">
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-md-12">
+                <form class="form-inline">
+                    <div class="input-group w-100">
+                        <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-success" type="submit">&#128269;</button> <!-- Magnifying glass symbol -->
+                        </div>
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container py-5">
+    <div class="row">
+        <!-- Your content goes here -->
+    </div>
+</div>
+
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="https://bmkltsly13vb.compat.objectstorage.ap-mumbai-1.oraclecloud.com/cdn.ft.lk/ftadmin/wp-content/uploads/2016/05/27210111/Cinnamon-Red-1.jpg" class="w-100" alt="Cinnamon Red Colombo">
+            </div>
+
+            <div class="col-md-8">
+                <h3>Cinnamon Red Colombo</h3>
+                <p>Kollupitiya, Colombo</p>
+<!--                <span class="badge badge-danger">In High Demand</span>-->
+<!--                <span class="badge badge-secondary">Great Value</span>-->
+                    <span class="badge badge-danger" style="background-color: #E15181;">In High Demand</span>
+                    <span class="badge badge-secondary" style="background-color: blue;">Great Value</span>
+                <p style="color: green; font-weight: bold; font-size: 24px;">LKR 15,000</p>
+                    <div class="col-md-12 text-right">
+                        <button type="button" class="btn btn-primary btn-sm">Book Now</button>
+                    </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-4">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/199251849.jpg?k=3fd832e1198aae54fafa7cecb7b0f91c1048480b533470cebb9b936d2f770fe5&o=&hp=1.jpg" class="w-100" alt="Fairway Colombo">
+            </div>
+            <div class="col-md-8">
+                <h3>Fairway Colombo</h3>
+                <p>Fort, Colombo</p>
+                <span class="badge badge-danger" style="background-color: #E15181;">In High Demand</span>
+                <div class="alert alert-warning" role="alert">
+                    You Missed it!<br> Your dates are Popular - We've run out of rooms at this property!
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
